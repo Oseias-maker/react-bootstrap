3157a8adfa96a2bb40dafda0bfca9f61ed956955
Lucca <mario-lucca@hotmail.com>
2021-04-12 16:56:46 -0300

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Table } from 'reactstrap';
+
+const Footer = (props) => {
+  return (
+    <Table borderless>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Primeiro Nome</th>
+          <th>Sobrenome</th>
+          <th>Nome de usuário</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mario</td>
+          <td>Padrao</td>
+          <td>@Luccazx12</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Patrick</td>
+          <td>Borges</td>
+          <td>@Patricksp08</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Oséias</td>
+          <td>Farias</td>
+          <td>@Oseias-maker</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default Footer;
